06d4cc6b2eb02b01200e09f1be4320e592181f05
Ganpiero <ganpieromaiketreyeszavaleta@gmail.com>
2025-12-20 07:57:41 -0500

    Módulo Auth: login sin layout y configuración de gitignore

--- Controllers/ReporteController.cs
+++ Controllers/ReporteController.cs
@@ -24,7 +24,7 @@ namespace AlmaHogarFront.Controllers
             {
                 PropertyNameCaseInsensitive = true
             };
-        }
+        }   
 
 
         public async Task<IActionResult> Index(string eleccion)
